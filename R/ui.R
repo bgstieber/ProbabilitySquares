524b337a209dde22cb40585a871e0553a11e3fcc
bgstieber <bgstieber@gmail.com>
2017-02-03 15:01:48 -0600

    added color descriptions

--- R/ui.R
+++ R/ui.R
@@ -20,13 +20,13 @@ shinyUI(fluidPage(
     #based on number of categories, we show the number of 
     # fields 
     numericInput('cat1',
-                 '% of Total for 1st Category',
+                 '% of Total for 1st Category (R)',
                  min = 0,
                  max = 100,
                  step = 1),
     
     numericInput('cat2',
-                 '% of Total for 2nd Category',
+                 '% of Total for 2nd Category (B)',
                  min = 0,
                  max = 100,
                  step = 1
@@ -34,7 +34,7 @@ shinyUI(fluidPage(
     
     conditionalPanel("input.cats >= 3",
     numericInput('cat3',
-                 '% of Total for 3rd Category',
+                 '% of Total for 3rd Category (G)',
                  min = 0,
                  max = 100,
                  step = 1
@@ -43,7 +43,7 @@ shinyUI(fluidPage(
     
     conditionalPanel("input.cats >= 4",
     numericInput('cat4',
-                 '% of Total for 4th Category',
+                 '% of Total for 4th Category (P)',
                  min = 0,
                  max = 100,
                  step = 1
@@ -52,7 +52,7 @@ shinyUI(fluidPage(
     
     conditionalPanel("input.cats >= 5",
     numericInput('cat5',
-                 '% of Total for 5th Category',
+                 '% of Total for 5th Category (O)',
                  min = 0,
                  max = 100,
                  step = 1
@@ -61,7 +61,7 @@ shinyUI(fluidPage(
     
     conditionalPanel("input.cats >= 6",
     numericInput('cat6',
-                 '% of Total for 6th Category',
+                 '% of Total for 6th Category (Y)',
                  min = 0,
                  max = 100,
                  step = 1
